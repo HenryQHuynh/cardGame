089e56373f68318bbc17ca9bbf3aef91207c7d0f
Henry Huynh <henryqhuynh@gmail.com>
2023-05-25 16:20:55 -0700

    Completed JS funtionality, added in funtions for cardMatch and cardMisMatch.

--- script.js
+++ script.js
@@ -70,11 +70,44 @@ class MixOrMatch {
          card.classList.add('visible');
 
          if(this.cardToCheck)
-            // checkformatch
+            this.checkForCardMatch(card);
          else
             this.cardToCheck = card;
       }
    }
+
+   checkForCardMatch(card) {
+      if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+         this.cardMatch(card, this.cardToCheck);
+      else
+         this.cardMisMatch(card, this.cardToCheck);
+
+      this.cardToCheck = null;
+   }
+
+   cardMatch(card1, card2) {
+      this.matchedCards.push(card1);
+      this.matchedCards.push(card2);
+      card1.classList.add('matched');
+      card2.classList.add('matched');
+      this.audioController.match();
+      if(this.matchedCards.length === this.cardsArray.length)
+         this.victory();
+   };
+
+   cardMisMatch(card1, card2) {
+      this.busy = true;
+      setTimeout(() => {
+         card1.classList.remove('visible');
+         card2.classList.remove('visible');
+         this.busy = false;
+      }, 1000);
+   }
+
+   getCardType(card) {
+      return card.getElementsByClassName('card-value')[0].src;
+   }
+
    startCountDown() {
       return setInterval(() => {
          this.timeRemaining--;
@@ -103,8 +136,7 @@ class MixOrMatch {
    };
    }
    canFlipCard(card) {
-      return true;
-      // return (!this.busy && !this.matchedCards.includes(card) && card != this.cardToCheck);
+      return (!this.busy && !this.matchedCards.includes(card) && card != this.cardToCheck);
    }
 }
 
